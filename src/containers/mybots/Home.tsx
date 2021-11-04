6368c98f1c875a2f3367cb3682ae9cd536581f37
Thomas Cadle <thomascadle@Thomass-MacBook-Pro.local>
2021-11-04 02:01:02 -0400

    cleaning

--- src/containers/mybots/Home.tsx
+++ src/containers/mybots/Home.tsx
@@ -1,26 +1,16 @@
 import { useEffect, useState } from "react";
 import styled from "styled-components";
-import Countdown from "react-countdown";
-import { Button, CircularProgress, Snackbar } from "@material-ui/core";
-import Alert from "@material-ui/lab/Alert";
+import { CircularProgress } from "@material-ui/core";
 import { Data } from "./bot_hashes.js";
 import { BotNft } from "../../components/botNft";
 import * as anchor from "@project-serum/anchor";
 import jQuery from 'jquery';
-
-import { LAMPORTS_PER_SOL, AccountInfo } from "@solana/web3.js";
-
+import { LAMPORTS_PER_SOL } from "@solana/web3.js";
 import { useAnchorWallet } from "@solana/wallet-adapter-react";
 import { WalletDialogButton } from "@solana/wallet-adapter-material-ui";
 import { programs } from "@metaplex/js";
+import { shortenAddress } from "./candy-machine";
 
-import {
-  CandyMachine,
-  awaitTransactionSignatureConfirmation,
-  getCandyMachineState,
-  mintOneToken,
-  shortenAddress,
-} from "./candy-machine";
 
 const ConnectButton = styled(WalletDialogButton)`
   padding: 16px 22px!important;
@@ -42,6 +32,7 @@ const ConnectButton = styled(WalletDialogButton)`
     color: #FFFFFF!important;
   }
 `;
+
 const Heading = styled.div`
   display: flex;
   flex-direction: column;
@@ -102,19 +93,6 @@ const DataContainer = styled.div`
   width: 275px;
 `;
 
-const MetadataContainer = styled.div`
-  padding: 10px;
-  margin: 15px;
-  position=relative;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  color: #FFFFFF!important;
-  border: 2px solid;
-  font-size: 14px!important;
-  font-family: Share Tech Mono!important;
-`;
-
 const MetadataWrapRow = styled.div`
   display: flex;
   flex-wrap: wrap;
@@ -126,27 +104,6 @@ const MetadataWrapRow = styled.div`
   }
 `;
 
-
-const MintButton = styled(Button)``;
-
-
-const TOKEN_PROGRAM_ID = new anchor.web3.PublicKey(
-  "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA"
-);
-
-const METADATA_PROGRAM_ID = new anchor.web3.PublicKey(
-  "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s"
-);
-
-// const MINT_KEY = new anchor.web3.PublicKey(
-//   "34s8cCyb77TQEojCHzxToPa6f9HarEXTPcyMT3GDsGYH"
-// );
-
-const {
-  metadata: { Metadata }
-} = programs;
-
-// const metadata = await Metadata.load(connection, metadataAccount);
 export const Details = styled.p`
   font-size: 20px;
   font-weight: 400;
@@ -163,10 +120,17 @@ export const Details = styled.p`
 }
 `;
 
-const BotImg = styled.img`
-  width: 12em;
-  height: 16em;
-`;
+const TOKEN_PROGRAM_ID = new anchor.web3.PublicKey(
+  "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA"
+);
+
+const METADATA_PROGRAM_ID = new anchor.web3.PublicKey(
+  "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s"
+);
+
+const {
+  metadata: { Metadata }
+} = programs;
 
 export interface HomeProps {
   connection: anchor.web3.Connection;
@@ -181,122 +145,72 @@ type BotType = {
 
 const Home = (props: HomeProps) => {
   const [balance, setBalance] = useState<number>();
-  const [metadataAccount, setMetadataAccount] = useState<string>();
-  const [metadataInfo, setMetadataInfo] = useState<string>();
-  const [uri, setUri] = useState<string>();
-  const [pngUri, setPngUri] = useState<string>();
   const [isLoaded, setIsLoaded] = useState(false);
   const [pngUriList, setPngUriList] = useState<any[]>();
   const [botMetadataList, setBotMetadataList] = useState<BotType[]>();
-  const [accountInfo, setAccountInfo] = useState<null | AccountInfo<Buffer>>();
-  const [walletTokens, setWalletTokens] = useState<any[]>();
-  const [botTokens, setBotTokens] = useState<any[]>();
-  const [alertState, setAlertState] = useState<AlertState>({
-    open: false,
-    message: "",
-    severity: undefined,
-  });
 
   const wallet = useAnchorWallet();
-
   const valid2DList = Data
-
   const png2DList : string[] = [];
-  const name2DList : string[] = [];
-
   const botData : BotType[] = [];
 
 
-  const getMetadata = () => {
-    (async () => {
+//   const getMetadata = () => {
+//     (async () => {
+//
+//     if (!wallet) return;
+//
+//     if (botTokens) {
+//     for (const id of botTokens) {
+//       setIsLoaded(false);
+//
+//       const MINT_KEY = new anchor.web3.PublicKey(
+//         id
+//       );
+//       const pdaAccount = await anchor.web3.PublicKey.findProgramAddress([Buffer.from('metadata'), METADATA_PROGRAM_ID.toBuffer(),MINT_KEY.toBuffer()],METADATA_PROGRAM_ID)
+//
+//       const metadata = await Metadata.load(props.connection, pdaAccount[0].toString());
+//
+//
+//       console.log(metadata)
+//       console.log(metadata.data.data.uri.toString())
+//
+//       const arweaveData = await jQuery.getJSON(metadata.data.data.uri.toString())
+//       const arweaveDataName = arweaveData.name
+//       const arweaveDataAttributes = arweaveData.attributes
+//
+//       // const arweaveDataArray
+//       console.log("arweave data")
+//       console.log(arweaveData)
+//       console.log(arweaveData.image)
+//       console.log("name")
+//       console.log(arweaveDataName)
+//       console.log("attributes")
+//       console.log(arweaveDataAttributes)
+//
+//       botData.push(
+//         {
+//           name: arweaveDataName,
+//           uri: arweaveData.image.toString(),
+//           attributes: arweaveDataAttributes
+//         }
+//       )
+//
+//       console.log("BOT DATA ARRAY")
+//       console.log(botData)
+//
+//       png2DList.push(arweaveData.image.toString())
+//       setBotMetadataList(botData)
+//       setPngUriList(png2DList)
+//
+//
+//       console.log("2D list")
+//       console.log(png2DList)
+//   }}
+//
+//   })();
+// };
 
-    if (!wallet) return;
-
-    if (botTokens) {
-    for (const id of botTokens) {
-
-      const MINT_KEY = new anchor.web3.PublicKey(
-        id
-      );
-      const pdaAccount = await anchor.web3.PublicKey.findProgramAddress([Buffer.from('metadata'), METADATA_PROGRAM_ID.toBuffer(),MINT_KEY.toBuffer()],METADATA_PROGRAM_ID)
-
-      setMetadataAccount(pdaAccount[0].toString())
-
-      const metadata = await Metadata.load(props.connection, pdaAccount[0].toString());
-
-      setMetadataInfo(metadata.toString())
-      setUri(metadata.data.data.uri.toString())
-
-      console.log(metadata)
-      console.log(metadata.data.data.uri.toString())
-
-      const arweaveData = await jQuery.getJSON(metadata.data.data.uri.toString())
-      const arweaveDataName = arweaveData.name
-      const arweaveDataAttributes = arweaveData.attributes
-      console.log("arweave data")
-      console.log(arweaveData)
-      console.log(arweaveData.image)
-      console.log("name")
-      console.log(arweaveDataName)
-      console.log("attributes")
-      console.log(arweaveDataAttributes)
-
-      setPngUri(arweaveData.image.toString())
-      png2DList.push(arweaveData.image.toString())
-
-      // setPngUriList(png2DList => [...png2DList, arweaveData.image.toString()]);
-      // setPngUriList({
-      //   png2DList: this.state.png2DList.concat(arweaveData.image.toString())
-      // })
-      // png2DList.concat(arweaveData.image.toString())
-
-      console.log("2D list")
-      console.log(png2DList)
-  }}
-
-  })();
-};
-
-  // const getArweaveData = (uri) => {
-  //   const arweaveData = jQuery.getJSON(uri)
-  //   console.log("arweave data")
-  //   console.log(arweaveData)
-  //   console.log(arweaveData.image)
-  //
-  //   setPngUri(arweaveData.image.toString())
-  //   // return arweaveData
-  // }
-
-  // useEffect(getMetadata, [wallet, props.connection]);
-  // useEffect(getArweaveData, [wallet, props.connection]);
-
-  // useEffect(() => {
-  //   getArweaveData(uri)
-  // });
-/*
-  useEffect(() => {
-  (async () => {
-    if (wallet) {
-
-      const pdaAccount = await anchor.web3.PublicKey.findProgramAddress([Buffer.from('metadata'), METADATA_PROGRAM_ID.toBuffer(),MINT_KEY.toBuffer()],METADATA_PROGRAM_ID)
-
-      console.log(pdaAccount[0].toString())
-      setMetadataAccount(pdaAccount[0].toString())
-
-      const metadata = await Metadata.load(props.connection, pdaAccount[0].toString());
-
-      console.log(metadata)
-      console.log(metadata.data.data.uri)
-      setMetadataInfo(metadata.toString())
-      setUri(metadata.data.data.uri.toString())
-
-
-      // const arweaveData = await jQuery.getJSON(metadata.data.data.uri)
-      // console.log(arweaveData)
-    }
-  })();
-  }, [props.connection]);
-*/
 
   useEffect(() => {
   (async () => {
@@ -334,8 +248,6 @@ const Home = (props: HomeProps) => {
 
       console.log("token addresses")
       console.log(tokenAddresses)
-      setBotTokens(botAddresses)
-      setWalletTokens(tokenAddresses)
 
       // if (botTokens) {
       for (const id of botAddresses) {
@@ -346,12 +258,8 @@ const Home = (props: HomeProps) => {
         );
         const pdaAccount = await anchor.web3.PublicKey.findProgramAddress([Buffer.from('metadata'), METADATA_PROGRAM_ID.toBuffer(),MINT_KEY.toBuffer()],METADATA_PROGRAM_ID)
 
-        setMetadataAccount(pdaAccount[0].toString())
-
         const metadata = await Metadata.load(props.connection, pdaAccount[0].toString());
 
-        setMetadataInfo(metadata.toString())
-        setUri(metadata.data.data.uri.toString())
 
         console.log(metadata)
         console.log(metadata.data.data.uri.toString())
@@ -361,13 +269,13 @@ const Home = (props: HomeProps) => {
         const arweaveDataAttributes = arweaveData.attributes
 
         // const arweaveDataArray
-        console.log("arweave data")
-        console.log(arweaveData)
-        console.log(arweaveData.image)
-        console.log("name")
-        console.log(arweaveDataName)
-        console.log("attributes")
-        console.log(arweaveDataAttributes)
+        // console.log("arweave data")
+        // console.log(arweaveData)
+        // console.log(arweaveData.image)
+        // console.log("name")
+        // console.log(arweaveDataName)
+        // console.log("attributes")
+        // console.log(arweaveDataAttributes)
 
         botData.push(
           {
@@ -377,25 +285,10 @@ const Home = (props: HomeProps) => {
           }
         )
 
-        console.log("BOT DATA ARRAY")
-        console.log(botData)
-
-        setPngUri(arweaveData.image.toString())
         png2DList.push(arweaveData.image.toString())
-        // name2DList.push()
+
         setBotMetadataList(botData)
         setPngUriList(png2DList)
-
-
-
-        // setPngUriList(png2DList => [...png2DList, arweaveData.image.toString()]);
-        // setPngUriList({
-        //   png2DList: this.state.png2DList.concat(arweaveData.image.toString())
-        // })
-        // png2DList.concat(arweaveData.image.toString())
-
-        console.log("2D list")
-        console.log(png2DList)
     }
 
     setIsLoaded(true);
@@ -433,11 +326,5 @@ const Home = (props: HomeProps) => {
     </main>
   );
 };
-
-interface AlertState {
-  open: boolean;
-  message: string;
-  severity: "success" | "info" | "warning" | "error" | undefined;
-}
 
 export default Home;
